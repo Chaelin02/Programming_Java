cb1404292d349f63505c7fa6908a4ae152a5a7be
Chaelin Won <wonchaelin6@gmail.com>
2019-09-06 09:07:48 +0900

    어쩌다 금요일

--- 00.06_GUI1/src/GUI1_JFrame.java
+++ 00.06_GUI1/src/GUI1_JFrame.java
@@ -3,6 +3,10 @@ import javax.swing.*;
 public class GUI1_JFrame  {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
+        JLabel lable = new JLabel("아싸 어쩌다 금요일");
+
+        frame.add(lable);
+
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    //X버튼 누르면 끝ㅐㄴ네
