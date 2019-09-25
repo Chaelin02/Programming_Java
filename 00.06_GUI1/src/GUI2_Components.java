fb7146908fe597ca52371ecc530f540499e9c548
Chaelin Won <wonchaelin6@gmail.com>
2019-09-25 09:28:09 +0900

    make showResult()

--- 00.06_GUI1/src/GUI2_Components.java
+++ 00.06_GUI1/src/GUI2_Components.java
@@ -52,20 +52,7 @@ JLabel lblResult = new JLabel("Result");
         bt0.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                String result = "";
-                result += lb0.getText();
-                result += bt0.getText();
-                result += cb0.isSelected();
-                result += cb1.isSelected();
-                result += rb0.isSelected();
-                result += rb1.isSelected();
-                result += tf0.getText();
-                result += tf0.getText();
-                result += cbox.getSelectedIndex();
-                result += cbox.getSelectedItem();
-                lblResult.setText(result);
-
-
+                showResult(lb0, bt0, cb0, cb1, rb0, rb1, tf0, ta0, cbox, lblResult);
 
 
             }
@@ -75,17 +62,22 @@ JLabel lblResult = new JLabel("Result");
         ItemListener il = new ItemListener() {   //cb0,cb1중 아무나 선택이 되면 여기il 로 들여와랴ㅏㅣ
             @Override
             public void itemStateChanged(ItemEvent e) {    //아이템이 눌리면 무조건 여기로 와라. 그다음에 얘가 알아서 판단함
-                if(e.getSource() == cb0 ){
-                    System.out.println("JCheckbox0 : "+cb0.isSelected());
-                }else if(e.getSource() == cb1){
-                    System.out.println("JCheckbox1 : "+cb1.isSelected());
-                }else if(e.getSource() == rb0){
-                    System.out.println("미성년자 : "+rb0.isSelected());
-                }else if(e.getSource() == rb1){
-                    System.out.println("성인 : "+rb1.isSelected());
-                }else if(e.getSource() == cbox){
-                    System.out.println(cbox.getSelectedItem());
-                }
+                showResult(lb0, bt0, cb0, cb1, rb0, rb1, tf0, ta0, cbox, lblResult);
+                //console 에는 표시가되ㅣ는데 Lable에는표시가 안되서 Method함수로 만들어서 호출함
+                //왜? 중복된 코드를 만들고 싶지 않아성
+
+
+//                if(e.getSource() == cb0 ){
+//                    System.out.println("JCheckbox0 : "+cb0.isSelected());
+//                }else if(e.getSource() == cb1){
+//                    System.out.println("JCheckbox1 : "+cb1.isSelected());
+//                }else if(e.getSource() == rb0){
+//                    System.out.println("미성년자 : "+rb0.isSelected());
+//                }else if(e.getSource() == rb1){
+//                    System.out.println("성인 : "+rb1.isSelected());
+//                }else if(e.getSource() == cbox){
+//                    System.out.println(cbox.getSelectedItem());
+//                }
             }
         };
     cb0.addItemListener(il);
@@ -112,5 +104,20 @@ JLabel lblResult = new JLabel("Result");
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private static void showResult(JLabel lb0, JButton bt0, JCheckBox cb0, JCheckBox cb1, JRadioButton rb0, JRadioButton rb1, JTextField tf0, JTextArea ta0, JComboBox cbox, JLabel lblResult) {
+        String result = "";
+        result += lb0.getText();
+        result += bt0.getText();
+        result += cb0.isSelected();
+        result += cb1.isSelected();
+        result += rb0.isSelected();
+        result += rb1.isSelected();
+        result += tf0.getText();
+        result += ta0.getText();
+        result += cbox.getSelectedIndex();
+        result += cbox.getSelectedItem();
+        lblResult.setText(result);
     }
 }
